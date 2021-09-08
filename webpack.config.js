ffaf9ef1f11f19b399b91342bca53bf73aa71d62
Federico Galan <fegv.2@hotmail.com.ar>
2021-09-08 09:57:17 -0300

    commit prueba

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    sources: false,
+                    minimize: false
+                },
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+
+}
